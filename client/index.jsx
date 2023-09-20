bf77075c29df1a1b1c78aaa4a4db07c34cf80dc1
Vegard Falldalen <vegfall@gmail.com>
2023-09-20 14:18:07 +0200

    hot module reload (HMR) with express

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>My react task application</h1>)
+root.render(<h1>My react task application with Express and reload</h1>)
