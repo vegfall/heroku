8133ac487c433b70670ee7a83a84509dcddccba5
Vegard Falldalen <vegfall@gmail.com>
2023-09-20 16:14:44 +0200

    Improvement: Can now update tasks *(But all tasks are update, and all title are set to Updated)

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function TaskListEntry({task}) {
+    async function handleUpdate() {
+        await fetch("/api/tasks", {
+            method: "PUT",
+            body: JSON.stringify({
+                    title: "new title"
+            }),
+            headers: {
+                "content-type": "application/json"
+            }
+        });
+    }
+
+    return <h2>
+        <input value={task.title}/>
+        <br/>
+        <button onClick={handleUpdate}>Update</button>
+    </h2>
+}
+
 function TaskApplication() {
     const [tasks, setTasks] = useState([]);
 
@@ -17,7 +37,7 @@ function TaskApplication() {
 
     return <div>
         <h1>My react task application with Express and reload</h1>
-        {tasks.map(t => <div>{t.title}</div>)}
+        {tasks.map(t => <TaskListEntry task={t} />)}
     </div>
 }
 
--- server/server.js
+++ server/server.js
@@ -2,19 +2,33 @@ import express from "express";
 
 const app = express();
 
+const TASKS = [
+    {
+        title: "task1",
+        status: "done"
+    },
+    {
+        title: "task2",
+        status: "begun"
+    },
+    {
+        title: "task3",
+        status: "future"
+    }
+];
 app.get("/api/tasks", (req, res) => {
     {
-        res.json(
-            [
-                {
-                    title: "task1"
-                },
-                {
-                    title: "task2"
-                }
-            ]
-        )
+        res.json(TASKS)
     }
+})
+
+app.put("/api/tasks", (req, res) => {
+    for (const task of TASKS) {
+        task.title = "Updated";
+    }
+
+    res.end();
+
 })
 
 app.use(express.static("../client/dist"));
